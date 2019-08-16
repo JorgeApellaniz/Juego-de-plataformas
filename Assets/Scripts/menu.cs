e782fbc7bc7d0b4fbaef3f3e51573d083adf3d77
JorgeApellaniz <jorgeapellaniz@usal.es>
2019-08-16 14:42:56 +0200

    Haciendo el menú

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	public void comenzar()
+	{
+		SceneManager.LoadScene ("Niveles");
+	}
+
+	public void salir()
+	{
+		Application.Quit ();
+	}
+
+	public void pasarANivel1()
+	{
+		SceneManager.LoadScene ("Lobby");
+	}
+
+	//TODO hacer escena 2
+	public void pasarANivel2()
+	{
+		SceneManager.LoadScene ("Escena2");
+	}
+		
+
+	public void instrucciones()
+	{
+		SceneManager.LoadScene ("instrucciones");
+	}
+
+	public void volverMenu()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+}
